cf6f94fdba03ca4c5e12f30118dbd0dce1a9818a
Stand <stanvmutafchiev@gmail.com>
2017-05-03 11:49:36 +0200

    Movement with some camera problems

---
+++ Assets/Scripts/Camera_Movement.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+public class Camera_Movement : MonoBehaviour {
+    [SerializeField]
+	private Transform _target;
+    [SerializeField]
+    private Vector3 _distanceVector = new Vector3 (0, 0, 0);
+    [SerializeField]
+    private Vector3 _distanceView = new Vector3 (0, 0, 0);
+    [SerializeField]
+    private float _smoothness = 0.1f;
+    private Vector3 _distanceOnExit = new Vector3(0, 0, 0);
+	private GameObject _dynamicTarget;
+	private GameObject _player;
+	private GameObject _camTarget;
+	private Vector3 _wantedDistance;
+	private Vector3 _currentDistance;
+	private Vector3 _wantedPosition;
+
+    public Transform Target {
+        get { return _target; }
+        set { _target = value; }
+    }
+    public Vector3 DistanceView
+    {
+        get { return _distanceView; }
+        set { _distanceView = value; }
+    }
+    public Vector3 DistanceOnExit
+    {
+        get { return _distanceOnExit; }
+        set { _distanceOnExit = value; }
+    }
+
+
+    void Start () {
+        _distanceOnExit = _distanceView;
+		_player = GameObject.FindGameObjectWithTag ("Player");
+		_camTarget = GameObject.CreatePrimitive (PrimitiveType.Sphere);
+		_camTarget.GetComponent<MeshRenderer> ().enabled = false;
+		_camTarget.GetComponent<SphereCollider> ().enabled = false;
+		_camTarget.name = "_camTarget";
+		_camTarget.transform.SetParent (transform);
+		_camTarget.transform.position = _player.transform.position;
+		_dynamicTarget = GameObject.CreatePrimitive (PrimitiveType.Sphere);
+		_dynamicTarget.GetComponent<MeshRenderer> ().enabled = false;
+		_dynamicTarget.GetComponent<SphereCollider> ().enabled = false;
+		_dynamicTarget.name = "_dynamicTarget";
+		_dynamicTarget.transform.position = _camTarget.transform.position;
+	}
+	void FixedUpdate () {
+		_camTarget.transform.position =_player.transform.position;
+		if (_target != null)
+			_wantedDistance = _target.position + _distanceView;
+		else
+			_wantedDistance = _camTarget.transform.position + _distanceVector;
+		_currentDistance = Vector3.Lerp (_currentDistance, _wantedDistance, _smoothness);
+		transform.position = _currentDistance;
+		if (_target != null)
+			_wantedPosition = _target.position;
+		else
+			_wantedPosition = _target.position + _distanceView;
+		_dynamicTarget.transform.position = Vector3.Lerp (_dynamicTarget.transform.position, 
+			_wantedPosition, _smoothness);
+	}	
+}
---
+++ Assets/Scripts/SubMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class SubMovement : MonoBehaviour {
+
+    private Rigidbody _rigidBody;
+    private Camera _camera;
+
+	void Start () {
+        _rigidBody = GetComponent<Rigidbody>();
+        _camera = Camera.main;
+	}
+
+	void Update () {
+        //Vector3 cursorPosition = _camera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, _camera.nearClipPlane));
+        //Debug.Log(cursorPosition);
+        //Vector3 gameObjectPos = _rigidBody.gameObject.transform.position;
+        //Vector3 direction = transform.position - cursorPosition;
+        if (Input.GetMouseButton(0))
+        {
+            Vector3 pos = Input.mousePosition;
+            pos.z = -Camera.main.transform.position.z;
+            pos = Camera.main.ScreenToWorldPoint(pos);
+            Vector3 dir = pos - transform.position;
+            float distance = Vector3.Distance(pos, transform.position);
+            dir = dir.normalized;
+            float angle = Mathf.Atan2(dir.x, dir.y) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.AngleAxis(angle, Vector3.up); // Turns Right
+            transform.rotation = Quaternion.AngleAxis(angle, Vector3.down); //Turns Left
+            _rigidBody.AddForce(dir * (distance / 10), ForceMode.VelocityChange);
+        }
+        
+    }
+}
