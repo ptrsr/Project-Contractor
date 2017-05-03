573392c6b3507bc5972019e41ba338f8894b1d94
Stand <stanvmutafchiev@gmail.com>
2017-05-03 14:25:02 +0200

    Camera and rotation fix

--- Assets/Scripts/CamTrigger.cs
+++ Assets/Scripts/CamTrigger.cs
@@ -1,28 +1,17 @@
 ﻿using UnityEngine;
 using System.Collections;
 public class CamTrigger : MonoBehaviour {
+
 	private GameObject cam;
 	public Vector3 distanceVector = new Vector3 (0, 0, 0);
-	public Vector3 targetOffset = new Vector3 (0, 0, 0);
-	private GameObject _triggerTarget;
     private Camera_Movement _camScript;
 	void Start () {
 		cam = GameObject.FindGameObjectWithTag("MainCamera");
-		_triggerTarget = GameObject.CreatePrimitive (PrimitiveType.Sphere);
-		_triggerTarget.GetComponent<MeshRenderer> ().enabled = false;
-		_triggerTarget.GetComponent<SphereCollider> ().enabled = false;
-		_triggerTarget.name = "_triggerTarget";
-		_triggerTarget.transform.localPosition = targetOffset;
-		_triggerTarget.transform.SetParent (transform);
         _camScript = cam.GetComponent<Camera_Movement>();
 
     }
-	void Update () {
-		_triggerTarget.transform.localPosition = targetOffset;
-	}
 	void OnTriggerStay (Collider col) {
 		if (col.tag == "Player") {
-            _camScript.DistanceOnExit = _camScript.DistanceView;
             _camScript.DistanceView = distanceVector;
 
 		}
--- Assets/Scripts/Camera_Movement.cs
+++ Assets/Scripts/Camera_Movement.cs
@@ -1,21 +1,16 @@
 ﻿using UnityEngine;
 using System.Collections;
 public class Camera_Movement : MonoBehaviour {
-    [SerializeField]
 	private Transform _target;
     [SerializeField]
-    private Vector3 _distanceVector = new Vector3 (0, 0, 0);
+    private Vector3 _distanceOnExit = new Vector3 (0, 0, 0);
     [SerializeField]
     private Vector3 _distanceView = new Vector3 (0, 0, 0);
     [SerializeField]
     private float _smoothness = 0.1f;
-    private Vector3 _distanceOnExit = new Vector3(0, 0, 0);
-	private GameObject _dynamicTarget;
 	private GameObject _player;
-	private GameObject _camTarget;
 	private Vector3 _wantedDistance;
 	private Vector3 _currentDistance;
-	private Vector3 _wantedPosition;
 
     public Transform Target {
         get { return _target; }
@@ -34,33 +29,16 @@ public class Camera_Movement : MonoBehaviour {
 
 
     void Start () {
-        _distanceOnExit = _distanceView;
 		_player = GameObject.FindGameObjectWithTag ("Player");
-		_camTarget = GameObject.CreatePrimitive (PrimitiveType.Sphere);
-		_camTarget.GetComponent<MeshRenderer> ().enabled = false;
-		_camTarget.GetComponent<SphereCollider> ().enabled = false;
-		_camTarget.name = "_camTarget";
-		_camTarget.transform.SetParent (transform);
-		_camTarget.transform.position = _player.transform.position;
-		_dynamicTarget = GameObject.CreatePrimitive (PrimitiveType.Sphere);
-		_dynamicTarget.GetComponent<MeshRenderer> ().enabled = false;
-		_dynamicTarget.GetComponent<SphereCollider> ().enabled = false;
-		_dynamicTarget.name = "_dynamicTarget";
-		_dynamicTarget.transform.position = _camTarget.transform.position;
-	}
+        _target = _player.transform;
+    }
 	void FixedUpdate () {
-		_camTarget.transform.position =_player.transform.position;
 		if (_target != null)
-			_wantedDistance = _target.position + _distanceView;
-		else
-			_wantedDistance = _camTarget.transform.position + _distanceVector;
-		_currentDistance = Vector3.Lerp (_currentDistance, _wantedDistance, _smoothness);
-		transform.position = _currentDistance;
-		if (_target != null)
-			_wantedPosition = _target.position;
-		else
-			_wantedPosition = _target.position + _distanceView;
-		    _dynamicTarget.transform.position = Vector3.Lerp (_dynamicTarget.transform.position, 
-			_wantedPosition, _smoothness);
+        {
+            _wantedDistance = _target.position + _distanceView;
+            _currentDistance = Vector3.Lerp(_currentDistance, _wantedDistance, _smoothness);
+            transform.position = _currentDistance;
+        }
+			
 	}	
 }
