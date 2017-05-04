7c7c949ed95227f671609eaed1d6d2806e17c78f
Ruud Peters <ruud_peters96@hotmail.com>
2017-05-04 13:13:42 +0200

    shitty water doesn't work

---
+++ Assets/Scripts/Fog.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+public class Fog : MonoBehaviour
+{
+    [Range(0f, 3f)]
+    public float intensity = 0.5f;
+
+    [SerializeField]
+    private Shader _shader;
+
+    [SerializeField]
+    private Color
+        _startColor,
+        _endColor;
+
+    private Material _material;
+
+    private void Start()
+    {
+        GetComponent<Camera>().depthTextureMode = DepthTextureMode.Depth;
+        _material = new Material(_shader);
+    }
+
+    private void OnRenderImage(RenderTexture src, RenderTexture dest)
+    {
+        float depth = -Camera.main.transform.position.y / 80;
+
+        if (_shader != null && depth > 0)
+        {
+            _material.SetFloat("_intensity", intensity);
+            _material.SetVector("_startColor", _startColor);
+            _material.SetFloat("_depth", depth);
+            Graphics.Blit(src, dest, _material);
+        }
+        else
+        {
+            Graphics.Blit(src, dest);
+        }
+    }
+}
---
+++ Assets/Shaders/Water/water4.0.shader
@@ -0,0 +1,51 @@
+﻿Shader "custom/water4.0"
+{
+  Properties 
+  {
+
+  }
+
+  SubShader 
+  {
+    Pass 
+    { 	
+	  Tags{ "LightMode" = "ShadowCaster" }
+	  ZWrite On
+	  ZTest LEqual
+
+      	CGPROGRAM
+      	#pragma vertex vert
+      	#pragma fragment frag
+      	#include "UnityCG.cginc"
+
+
+      	struct appdata {
+      		float4 vertex : POSITION;
+      		float2 uv : TEXCOORD0;
+      	};
+   	
+      	struct v2f 
+      	{
+        	float4 pos : SV_POSITION;
+        	float2 uv : TEXCOORD0;
+      	};
+       
+      	v2f vert(appdata v)
+      	{         
+        	v2f o;
+        	o.pos = mul(UNITY_MATRIX_MVP, v.vertex);
+        	o.uv = v.uv;
+
+        	// calculating viewdirection on vertex
+
+        	return o;
+      	}
+
+      	fixed4 frag(v2f i) : COLOR 
+      	{
+			return half4(1,1,1,1);
+      	}
+      	ENDCG
+    	}
+  	}
+}
