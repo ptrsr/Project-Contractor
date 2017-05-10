c658d7c8828c74982dabc8a63022a86e0f42dab0
Leon Hup <Flappie136@live.nl>
2017-05-10 14:03:07 +0200

    Metas

---
+++ Assets/Scripts/Particles/Fish/FishParticles.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class FishParticles : MonoBehaviour
+{
+    private ParticleSystem _fish;
+    private ParticleSystem.Particle[] _particles;
+    
+    public void Start()
+    {
+        _fish = GetComponent<ParticleSystem>();
+        _fish.GetParticles(_particles);
+    }
+
+    public void Update()
+    {
+
+        var main = _fish.main;
+
+    }
+}
