f1ef653df243453f3e745c4c2ab4655bafebae2c
Leon Hup <Flappie136@live.nl>
2017-05-17 15:21:05 +0200

    no message

--- Assets/Scripts/AI/Fish/Type/Neutral/ElectricEel.cs
+++ Assets/Scripts/AI/Fish/Type/Neutral/ElectricEel.cs
@@ -6,27 +6,44 @@ public class ElectricEel : FishNeutral
 {
     [Header("Electric Eel Variables")]
     [SerializeField]
+    private int _chargeSpeed = 400;
+    public int ChargeSpeed { get { return _chargeSpeed; } }
+
+    [SerializeField]
     private Transform _hole = null;
     public Transform Hole { get { return _hole; } }
 
     private Transform _holeExit;
     public Transform HoleExit { get { return _holeExit; } }
 
+    private Vector3 _anchorOrigPos;
+    public Vector3 AnchorOrigPos { get { return _anchorOrigPos; } }
+
+    private Rigidbody _anchorBody;
+    public Rigidbody AnchorBody { get { return _anchorBody; } }
+    
     [SerializeField]
-    private float _knockbackStrength = 100f;
+    private Transform _anchor = null;
+    public Transform Anchor { get { return _anchor; } }
 
     [SerializeField]
-    private float _knockUpStrength = 25f;
+    private float _knockbackStrength = 100f;
 
     private Collider _collider;
     public Collider Collider { get { return _collider; } }
 
     public override void Start()
     {
-        base.Start();
+        origPos = transform.position;
+        origRot = transform.rotation;
+        body = GetComponent<Rigidbody>();
+
+        _anchorOrigPos = _anchor.position;
+        _anchorBody = _anchor.GetComponent<Rigidbody>();
+
+        _collider = GetComponent<Collider>();
 
         _target = FindObjectOfType<SubMovement>().transform;
-        _collider = GetComponent<Collider>();
 
         _holeExit = _hole.GetComponentsInChildren<Transform>()[1];
 
@@ -53,7 +70,8 @@ public class ElectricEel : FishNeutral
 
         //Stun player
 
-        c.rigidbody.AddForce(new Vector3(Direction.x * _knockbackStrength, _knockUpStrength), ForceMode.Impulse);
+        c.rigidbody.AddForce(Direction * _knockbackStrength, ForceMode.Impulse);
+        SetState<FishStateReturnToHole>();
     }
 
     public override Quaternion GetLookRotation(Vector3 direction)
