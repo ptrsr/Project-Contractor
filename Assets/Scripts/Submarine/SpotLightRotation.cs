4b91f77b1e6c995e41c240b73e7a8c6e41477183
Ruud Peters <ruud_peters96@hotmail.com>
2017-05-21 16:28:26 +0200

    temp

---
+++ Assets/Scripts/Submarine/SpotLightRotation.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpotLightRotation : MonoBehaviour
+{
+    private Camera _mainCamera;
+
+	void Start ()
+    {
+        _mainCamera = Camera.main;
+	}
+	
+	void Update ()
+    {
+        Vector3 mouseWorldPos = Camera.main.ViewportToWorldPoint(Input.mousePosition + new Vector3(0,0,1));
+
+	}
+}
