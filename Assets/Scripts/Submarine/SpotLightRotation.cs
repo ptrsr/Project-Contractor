4e623565afdd5fbf0289b2e3b6b42fc93ec548ee
Ruud Peters <ruud_peters96@hotmail.com>
2017-05-21 16:43:37 +0200

    temp

--- Assets/Scripts/Submarine/SpotLightRotation.cs
+++ Assets/Scripts/Submarine/SpotLightRotation.cs
@@ -13,7 +13,12 @@ public class SpotLightRotation : MonoBehaviour
 	
 	void Update ()
     {
-        Vector3 mouseWorldPos = Camera.main.ViewportToWorldPoint(Input.mousePosition + new Vector3(0,0,1));
+        Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition + new Vector3(0,0,1));
 
+        Vector3 delta = Vector3.Normalize(mouseWorldPos - transform.position);
+
+        float angle = Mathf.Atan2(delta.y, delta.x) * Mathf.Rad2Deg;
+
+        transform.eulerAngles = new Vector3(angle, 90, 0);
 	}
 }
