b1eee7593e032c7242ae6b0fada253408a1b9b72
Anton Volkholz <anton.volkholz@me.com>
2017-06-07 10:58:38 +0200

    no message

---
+++ Assets/Shaders/alt Sonar/camSonar.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camSonar : MonoBehaviour {
+
+	private Material mat;
+
+	private Camera mainCam;
+	private Camera sonarCam;
+
+	private RenderTexture scene;
+
+	void Start () {
+		mainCam = Camera.main;
+		sonarCam = GetComponent<Camera> ();
+		sonarCam.depthTextureMode = DepthTextureMode.Depth;
+		scene = new RenderTexture (sonarCam.pixelWidth, sonarCam.pixelHeight, 16, RenderTextureFormat.Depth);
+
+		mat = mainCam.GetComponent<FX> ()._mat;
+	}
+
+	void Update () {
+		CamPosition ();
+		RenderScene ();
+	}
+
+	void CamPosition () {
+		transform.position = mainCam.transform.position;
+		transform.rotation = transform.rotation;
+	}
+		
+	void RenderScene () {
+		sonarCam.targetTexture = scene;
+		sonarCam.Render ();
+		mat.SetTexture ("sonarScene", scene);
+	}
+}
---
+++ Assets/Shaders/alt Sonar/secondRender.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class secondRender : MonoBehaviour {
+
+	public Material mat;	
+
+	private Camera mainCam;
+
+	private RenderTexture scene;
+
+	void Start () {
+		mainCam = Camera.main;
+		scene = new RenderTexture (mainCam.pixelWidth, mainCam.pixelHeight, 16, RenderTextureFormat.Depth);
+	}
+
+	void Update () {
+		RenderScene ();
+	}
+
+	void RenderScene () {
+		mainCam.cullingMask = 8;
+		mainCam.targetTexture = scene;
+		mainCam.Render ();
+
+		mat.SetTexture ("sonarScene", scene);
+
+		mainCam.cullingMask = 1;
+		mainCam.targetTexture = null;
+	}
+}
