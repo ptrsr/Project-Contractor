d2fb150fe8124f35333a38df8335538cfdb76e17
Dylan <emotdj@gmail.com>
2017-06-16 15:21:37 +0100

    - Oxygen value tweaked

--- Assets/Scripts/AI/Fish/Behaviours/SharkReturn.cs
+++ Assets/Scripts/AI/Fish/Behaviours/SharkReturn.cs
@@ -119,6 +119,8 @@ public class SharkReturn : FishState
         int id = _shark.GetWayPointId(point);
         int startId = _shark.GetWayPointId(point);
 
+		int count = 0;
+
         //Try to find a waypoint in front of the shark to prevent back tracking
         do
         {
@@ -133,7 +135,8 @@ public class SharkReturn : FishState
                 _shark.WayId = id + 1 >= _shark.WayPoints.Length - 1 ? 0 : id + 1;
                 break;
             }
+			count++;
             //Check if a waypoint is in front of the shark
-        } while (Vector3.Angle(-_shark.transform.right, point.position - _shark.transform.position) > 45f);
+		} while (Vector3.Angle(-_shark.transform.right, point.position - _shark.transform.position) > 45f || count != _shark.WayPoints.Length);
     }
 }
