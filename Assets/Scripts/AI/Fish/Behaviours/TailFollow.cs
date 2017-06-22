5674157b7d349a7fd88c9760ea5385f85803d306
Leon Hup <Flappie136@live.nl>
2017-06-22 13:13:46 +0200

    - New Eel model - Eel textures - Eel behaviour fix - Shark no longer detects through walls - Shark rig animation

---
+++ Assets/Scripts/AI/Fish/Behaviours/TailFollow.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class TailFollow : FishState
+{
+    public TailFollow(Fish pFish) : base(pFish) { }
+
+    private SharkTail _tail;
+
+    private int _id = 0;
+
+    private float _headWobble = -1f;
+    private float _normalCap = 1f;
+    private float _chaseCap = 0.5f;
+    private float _changeVal = 0.1f;
+
+    public override void Initialize()
+    {
+        _tail = (SharkTail)fish;
+    }
+
+    public override void Step()
+    {
+        //Head counter
+        _headWobble += _changeVal;
+
+        //Switch direction on cap values
+        if (_headWobble <= (_tail.SharkValues.IsChasing ? -_chaseCap : -_normalCap))
+        {
+            _headWobble = _tail.SharkValues.IsChasing ? -_chaseCap : -_normalCap;
+            _changeVal = -_changeVal;
+        }
+        else if (_headWobble >= (_tail.SharkValues.IsChasing ? _chaseCap : _normalCap))
+        {
+            _headWobble = _tail.SharkValues.IsChasing ? _chaseCap : _normalCap;
+            _changeVal = -_changeVal;
+        }
+
+        //Add torque
+        _tail.Head.AddTorque(0, _tail.SharkValues.IsChasing ? _headWobble * _tail.ChaseSpeed : _headWobble * _tail.MoveSpeed, 0, ForceMode.Acceleration);
+
+        //Keep original rotation to prevent odd rotated tail
+        _tail.Tail.rotation = Quaternion.Slerp(_tail.Tail.rotation, _tail.OriginRot, _tail.RotationSpeed);
+
+        //Switch between left and right movement
+        if (Vector3.Distance(_tail.Tail.position, _tail.Positions[_id].position) < 6.4f)
+            _id = _id == 0 ? 1 : 0;
+
+        //Move towards swimming point
+        _tail.Direction = (_tail.Positions[_id].position - _tail.Tail.position).normalized;
+        _tail.Tail.AddForce(_tail.Direction * (_tail.SharkValues.IsChasing ? _tail.ChaseSpeed : _tail.MoveSpeed));
+    }
+}
---
+++ Assets/Test.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    float i = -1;
+    float add = 0.1f;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        i += add;
+        if (i >= 1 || i <= -1)
+            add = -add;
+        Rigidbody body = GetComponent<Rigidbody>();
+        body.AddTorque(0, i, 0, ForceMode.VelocityChange);
+    }
+}
